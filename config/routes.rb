53b67724632342e4d12e45d6a476f92062afff43
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-19 19:21:57 -0800

    Run command: rails g controller users my_portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
   devise_for :users
   root :to => 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
